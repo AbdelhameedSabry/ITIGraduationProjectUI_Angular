b929cd9bec5982086f4bc968e0653e6aaaa415b5
abdelhameedsabry <abdelhameedx95@gmail.com>
2022-11-15 16:13:47 +0200

    loginn update

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,6 +1,7 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, enableProdMode, ErrorHandler, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import { UsersService } from 'src/app/_services/users.service';
 import { setCookie, getCookie } from 'typescript-cookie';
 
@@ -9,23 +10,12 @@ import { setCookie, getCookie } from 'typescript-cookie';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, AfterViewInit, AfterContentInit {
+export class LoginComponent implements OnInit {
 
   form!: FormGroup;
-  token:any
-  @ViewChild("logginmessages")  element!:ElementRef
-  flag:boolean = false
-  constructor(private fb: FormBuilder, private usersSer:UsersService, private router:Router) { }
-  ngAfterContentInit(): void {
-    this.ngAfterViewInit()
-  }
-
-  ngAfterViewInit() {
-    if(this.flag == true){
-      this.element.nativeElement.style.display = "block";
-    }
-    console.log(this.flag);
-  }
+  token: any
+  flag: boolean = false
+  constructor(private fb: FormBuilder, private usersSer: UsersService, private router: Router) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -35,21 +25,25 @@ export class LoginComponent implements OnInit, AfterViewInit, AfterContentInit {
     })
   }
 
-
   login() {
-    this.usersSer.login(this.form.value).subscribe(t => {
-      this.token = t;
-      console.log(this.token)
-      setCookie("token",this.token.token); 
-    },
-    errorr => {
-      this.flag = true;
-      alert("Invalid username or password!");
-    },
-    () => {
-      this.router.navigateByUrl("/Home");
-      //alert("Logged in successfully!");
-    })
+    this.usersSer.login(this.form.value)
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error)
+        })
+      )
+      .subscribe({
+        next: (response) => {
+          this.token = response;
+          setCookie("token", this.token.token);
+        },
+        error: (error) => {
+          if (error.status == 400)
+            this.flag = true
+        },
+        complete: () => {
+          this.router.navigateByUrl('/Home')
+        }
+      })
   }
-
 }
